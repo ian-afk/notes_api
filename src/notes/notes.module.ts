4376a6455d56511979a307c8985e17e3fc2ba10c
ian-afk <crkdosal@gmail.com>
2025-11-12 22:53:11 +0800

    basic implementation of db connection

--- src/notes/notes.module.ts
+++ src/notes/notes.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { NotesService } from './notes.service';
 import { NotesController } from './notes.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Notes, NotesSchema } from './schemas/note.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Notes.name, schema: NotesSchema }]),
+  ],
   controllers: [NotesController],
   providers: [NotesService],
 })
