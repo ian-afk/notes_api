6cea93debad7c95fcfed1329e553dcaccd0a5342
ian-afk <crkdosal@gmail.com>
2025-11-12 20:11:28 +0800

    initial state of notes module

---
+++ src/notes/notes.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { CreateNoteDto } from './dto/create-note.dto';
+import { UpdateNoteDto } from './dto/update-note.dto';
+import { Note } from './entities/note.entity';
+
+@Injectable()
+export class NotesService {
+  create(createNoteDto: CreateNoteDto) {
+    return 'This action adds a new note';
+  }
+
+  findAll() {
+    return `This action returns all notes`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} note`;
+  }
+
+  update(id: number, updateNoteDto: UpdateNoteDto) {
+    return `This action updates a #${id} note`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} note`;
+  }
+}
