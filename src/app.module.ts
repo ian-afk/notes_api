df1ed13d294ecf4f8c4ca8e3dd77464bf5dc3edd
ian-afk <crkdosal@gmail.com>
2025-11-13 12:06:37 +0800

    add usermodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NotesModule } from './notes/notes.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+// import { UsersModule } from './users/users.module';
+// import { GoogleStrategy } from './google.strategy';
 
 @Module({
-  imports: [NotesModule, MongooseModule.forRoot('mongodb://localhost/notes')],
+  imports: [
+    NotesModule,
+    UsersModule,
+    MongooseModule.forRoot('mongodb://localhost/notes'),
+    ConfigModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
