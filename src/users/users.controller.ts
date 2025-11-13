e804afdc95618e55481c9d82d1012863612f0a97
ian-afk <crkdosal@gmail.com>
2025-11-13 14:16:44 +0800

    add get method for users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 
@@ -11,5 +11,14 @@ export class UsersController {
     const { email, password } = createUserDto;
 
     return this.usersService.create({ email, password });
+  }
+
+  @Get()
+  async findAll() {
+    const users = await this.usersService.findAll();
+    return {
+      status: 'success',
+      data: users,
+    };
   }
 }
