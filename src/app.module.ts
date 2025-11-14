e2249e3ad1ae5f2fa6548c2d503574e96da358e2
ian-afk <crkdosal@gmail.com>
2025-11-14 13:53:18 +0800

    move up config global

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,20 @@ import { NotesModule } from './notes/notes.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
-// import { UsersModule } from './users/users.module';
-// import { GoogleStrategy } from './google.strategy';
+
+import { AuthModule } from './auth/auth.module';
+import constants from './auth/constants';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [constants],
+    }),
     NotesModule,
     UsersModule,
+    AuthModule,
     MongooseModule.forRoot('mongodb://localhost/notes'),
-    ConfigModule.forRoot(),
   ],
   controllers: [AppController],
   providers: [AppService],
